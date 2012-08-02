532344702e57614795bc78a76d9db6d7e6015ac3
tokuhirom <tokuhirom@gmail.com>
2012-08-02 15:26:50 +0900

    - better trace - less operation

--- lib/Damail.pm
+++ lib/Damail.pm
@@ -14,12 +14,20 @@ use Encode::IMAPUTF7;
 
 use Damail::JSONMaker::DataPage;
 use Damail::JSONMaker::NetIMAPClient;
-use Class::Method::Modifiers qw(install_modifier);
 
+use Class::Method::Modifiers qw(install_modifier);
 use Log::Minimal;
 
 install_modifier('Net::IMAP::Client', 'before', '_socket_write', sub {
-    infof("[IMAP] %s", $_[1]);
+    local $Log::Minimal::TRACE_LEVEL = $Log::Minimal::TRACE_LEVEL + 1;
+    infof("[IMAP-SEND] %s", $_[1]);
+});
+install_modifier('Net::IMAP::Client', 'around', '_socket_getline', sub {
+    my $code = shift;
+    my $ret = $code->(@_);
+    local $Log::Minimal::TRACE_LEVEL = $Log::Minimal::TRACE_LEVEL + 1;
+    infof("[IMAP-RECV] %s", $ret);
+    return $ret;
 });
 
 sub create_client {
@@ -33,14 +41,12 @@ sub create_client {
 
     my $imap = Net::IMAP::Client->new(
         server => $conf->{imap_server},
-        user => $conf->{imap_user},
-        pass => $conf->{imap_pass},
-        ssl => $conf->{imap_ssl},
-        port => $conf->{imap_port},
+        user   => $conf->{imap_user},
+        pass   => $conf->{imap_pass},
+        ssl    => $conf->{imap_ssl},
+        port   => $conf->{imap_port},
     ) or die "Cannot connect to IMAP server";
     $imap->login or die "Cannot login: " . $imap->last_error;
-    my @folders = $imap->folders;
-    $imap->select('INBOX');
     return $imap;
 }
 
